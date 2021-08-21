f8f0dc1c52f52bc0d44c7566e04c997f48a3f7af
fabian <f.markert87@googlemail.com>
2021-08-21 18:29:33 +0200

    Questions eingebaut

--- src/main/kotlin/de/impro/repository/QuestionRepository.kt
+++ src/main/kotlin/de/impro/repository/QuestionRepository.kt
@@ -3,12 +3,18 @@ package de.impro.repository
 import de.impro.dto.SuggestionGroupedDto
 import de.impro.model.Question
 import io.micronaut.data.annotation.Query
+import io.micronaut.data.annotation.QueryHint
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.model.query.builder.sql.SqlQueryConfiguration
 import io.micronaut.data.repository.CrudRepository
+import org.hibernate.jpa.QueryHints
 import java.math.BigInteger
 
 @Repository
 interface QuestionRepository : CrudRepository<Question, BigInteger>{
+
+    @Query("select q from Question q  order by q.inputTime desc ")
+    @io.micronaut.data.annotation.repeatable.QueryHints( value = [QueryHint(name = QueryHints.HINT_FETCH_SIZE, value =  "1"), QueryHint(name = QueryHints.HINT_CACHEABLE, value = "true")])
+    fun findLatestQuestion():Question?
 
 }
